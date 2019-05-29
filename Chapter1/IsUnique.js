7882e4239699dea3025abb676bc224b9260dfa2b
Rashmi Prakash <rashmiprakash21@hotmail.com>
2019-05-29 10:08:05 -0400

    is Unique solved

---
+++ Chapter1/IsUnique.js
@@ -0,0 +1,27 @@
+//Implement an algorithm to determine if a string has all unique characrters. What if you cannot use additional data structures
+
+// using another data structure
+const unique = str => {
+  let newObj = {};
+  for (let char of str) {
+    if (!newObj[char]) {
+      newObj[char] = 1;
+    } else {
+      return false;
+    }
+    return true;
+  }
+}
+  console.log(unique('dabcdde'))
+  // without using another data structure
+  const unique1 = str1 => {
+    if (str1.length > 128) return false; //128 is the number of ASCII characters
+    let str2 = str1.split('').sort();
+    //console.log(str1)
+    for (let i = 0; i < str2.length; i++) {
+      if (str2[i] === str2[i + 1]) return false;
+    }
+    return true;
+  };
+  console.log(unique1('dfzerwhobnyq'))
+
